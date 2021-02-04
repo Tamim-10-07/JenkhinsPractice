2f5318be607839ad7fbc46f8d03ecf94685123a8
Thameem Ansari <71844464+Tamim-10-07@users.noreply.github.com>
2021-02-04 23:24:46 +0530

    Updated

--- Sample.java
+++ Sample.java
@@ -3,13 +3,11 @@ public class Sample
 
 public static void main(String[] args){
 
-System.out.println("Hello Thameem");
-System.out.println("Hello Tamim");
-System.out.println("Hello Thamem");
+
 System.out.println("Hello Tameem");
 System.out.println("Hello Tameem Ansari");//new Added
-  
-  System.out.println("Hello");//new Added_1
+
+  System.out.println("Its from SubBranch");//new Added_2
 
 }
 
