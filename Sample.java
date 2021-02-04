17e8e126b11829b6873bab8b12d9aa4e8e87d32d
Thameem Ansari <71844464+Tamim-10-07@users.noreply.github.com>
2021-02-04 23:17:19 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -7,6 +7,7 @@ System.out.println("Hello Thameem");
 System.out.println("Hello Tamim");
 System.out.println("Hello Thamem");
 System.out.println("Hello Tameem");
+  System.out.println("Hello Tameem Ansari");//new Added
 
 }
 
