05a785421bdc8e1e0c235ebda18aa716b463699f
Thameem Ansari <71844464+Tamim-10-07@users.noreply.github.com>
2021-02-04 22:57:48 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,13 @@
+public class Sample
+{
+
+public static void main(String[] args){
+
+System.out.println("Hello Thameem");
+System.out.println("Hello Tamim");
+System.out.println("Hello Thamem");
+System.out.println("Hello Tameem");
+
+}
+
+}
